1ff5ba80f36747ff5c45d3fb907a902a2d0d5e0a
Andreas Marr <andreasm@artcom.de>
2014-02-21 18:13:02 +0100

    documented a few things in deploy.rb

--- services/filecache/config/deploy.rb
+++ services/filecache/config/deploy.rb
@@ -12,19 +12,26 @@ set :stages, %w(production staging appliance)
 set :application, 'filecache'
 
 set :scm, :none
+# We deploy the content of this directory!
 set :repository, 'cache'
 set :repository_absolute_path, File.join(File.expand_path('../', File.dirname(__FILE__)), repository)
 
 set :deploy_via, :copy
-set :keep_releases, 5
+# Keep the last 25 releases. Any older releases are deleted
+set :keep_releases, 25
 
+# Run the deployment as this user
 set :user, 'deployment'
+# The services are run as this user
 set :runner, 'talk'
 set :use_sudo, true
 
+# Where on the server is the service deployed
 set :deploy_to, "/home/#{runner}/#{application}"
 
+# The Github repository which contains our releases
 set :github_repository, 'hoccer/hoccer-talk-spike'
+# The name of the product. Has to coincide with the git tag name
 set :product_name, application
 
 # Latest release if nil, specified version otherwise
@@ -32,10 +39,14 @@ set :product_name, application
 # e.g. `cap appliance deploy -s product_version=1.0.0`
 set :product_version, nil
 
-set :shared_children, %w(log)
+# Directories that are shared between releases.
+set :shared_children, %w(log conf)
 
-# Custom Recipe Hooks
+## Custom Recipe Hooks
+# The deploy:setup behaves oddly historically - fix this
 after 'deploy:setup', 'misc:fix_permissions'
+
+# Trigger cleanup of old releases
 after 'deploy', 'deploy:cleanup'
 
 before 'deploy:update', 'release:fetch'
