7364a5ce58e8cec5bd16a1c14ec093e070de6651
Kristine <kristine@artcom.de>
2014-03-06 11:25:34 +0100

    talktool: pass through command line arguments

--- services/talktool/files/talktool.sh
+++ services/talktool/files/talktool.sh
@@ -3,5 +3,5 @@
 echo "starting hoccer command-line-client..."
 
 #cat commands.conf | java -XX:-MaxFDLimit -Xmx12880m -jar jar/talktool.jar
-java -XX:-MaxFDLimit -Xmx38640m -jar jar/talktool.jar
+java -XX:-MaxFDLimit -Xmx38640m -jar jar/talktool.jar $*
 
