49b08244bd329da1b04dc90c0ac845d17416fddf
Martin Skinner <martin@artcom.de>
2014-03-07 14:36:38 +0100

    (talktool) added role "slave" for the test-client host - slaves are further users (talk0...talk9) with their own configs to allow parallel execution of the talktool for load testing.

--- services/talktool/config/deploy.rb
+++ services/talktool/config/deploy.rb
@@ -17,6 +17,7 @@ set :repository_absolute_path, File.join(File.expand_path('../', File.dirname(__
 set :jar_path, "#{repository_absolute_path}/jar"
 
 set :deploy_via, :copy
+set :copy_remote_dir, "/tmp"
 # Keep the last 25 releases. Any older releases are deleted
 set :keep_releases, 25
 
@@ -42,7 +43,10 @@ set :artifact_path, nil
 
 ## Custom Recipe Hooks
 # The deploy:setup behaves oddly historically - fix this
-after 'deploy:setup', 'misc:fix_permissions'
+# after 'deploy:setup', 'misc:fix_permissions'
+
+# after deploying the app master, symlink all the slaves to its assets
+after 'deploy:create_symlink', 'scaling:create_symlinks'
 
 # Trigger cleanup of old releases
 after 'deploy', 'deploy:cleanup'
@@ -74,5 +78,21 @@ namespace :scaling do # TODO: find a better namespace name
   
   task :link_executable do
     run_locally %Q|cd #{jar_path}; ln -s #{File.basename(artifact_path)} #{application}.jar|
+  end
+
+  task :create_symlinks, :roles => :slave do
+    commands = []
+    slave_current = "~/#{application}/current"
+    slave_shared = "~/#{application}/shared"
+   
+    commands << "rm -rf #{slave_current}"
+    commands << "mkdir #{slave_current}"
+    commands << "cd #{current_path};for file in *;do if [ ! -L $file ];then ln -s #{current_path}/$file #{slave_current}/$file;fi;done"
+    shared_children.map do |dir|
+      d = dir.shellescape
+      commands << "rm -rf -- #{slave_current}/#{d}"
+      commands << "ln -s -- #{slave_shared}/#{dir.split('/').last.shellescape} #{slave_current}/#{d}"
+    end
+    run commands.join(' && ') if commands.any?
   end
 end
